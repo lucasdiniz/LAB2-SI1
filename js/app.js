578e7f9d47f5b4467519c514403f09cee592e648
Lucas <lucazdinis@gmail.com>
2016-11-24 14:21:37 -0300

    New Button

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ app.controller('controller', function($scope){
     $scope.action = function(newName){
         this.isDone = false;
         this.name = newName;
-    }
+    };
 
     $scope.total = 0;
     $scope.done = 0;
@@ -24,7 +24,7 @@ app.controller('controller', function($scope){
             }
         }
 
-        $scope.actionList.push(new $scope.action($scope.actionName));
+            $scope.actionList.push(new $scope.action($scope.actionName));
         $scope.total = $scope.total + 1;
         $scope.clearForm();
 
@@ -34,17 +34,23 @@ app.controller('controller', function($scope){
         $scope.actionName = "";
     };
 
-    $scope.changeStatus = function(actionDone){
-
-        var index = $scope.actionList.indexOf(actionDone);
-
-        if($scope.actionList[index].isDone)
-            $scope.done = $scope.done - 1;
-        else
-            $scope.done = $scope.done + 1;
-
-        $scope.actionList[index].isDone = !$scope.actionList[index].isDone;
+    $scope.allDone = function(){
+        for(var i = 0 ; i < $scope.actionList.length ; i = i + 1) {
+            if (!$scope.actionList[i].isDone) {
+                $scope.done = $scope.done + 1;
+            }
+            $scope.actionList[i].isDone = true;
+        }
     };
+
+
+    $scope.calcDone = function(){
+        var sumDone = 0;
+        for(var i = 0 ; i < $scope.actionList.length ; i = i + 1) {
+            if($scope.actionList[i].isDone) sumDone += 1;
+        }
+        return sumDone;
+    }
 
 
 });
